8e7c192053b5276557b41d14b67ae030ee9c1cc8
Falcon3590 <118375336+Falcon3590@users.noreply.github.com>
2023-01-05 23:35:38 +0300

    Homework4

--- Homework1/Program.cs
+++ Homework1/Program.cs
@@ -3,3 +3,19 @@
 // a = 5; b = 7 -> max = 7
 // a = 2 b = 10 -> max = 10
 // a = -9 b = -3 -> max = -3
+
+Console.WriteLine("Введите число: ");
+
+int firstnumber = int.Parse(Console.ReadLine());
+int secondnumber = int.Parse(Console.ReadLine());
+
+if (firstnumber > secondnumber)
+{
+Console.WriteLine("{0} наибольшее число", firstnumber);
+Console.WriteLine("{0} наименьшее число ", secondnumber);
+}
+else
+{
+Console.WriteLine("{0} наибольшее число ", secondnumber);
+Console.WriteLine("{0} наименьшее число ", firstnumber);
+}
--- Homework2/Program.cs
+++ Homework2/Program.cs
@@ -3,3 +3,16 @@
 // 2, 3, 7 -> 7
 // 44 5 78 -> 78
 // 22 3 9 -> 22
+
+Console.WriteLine("Введите число: ");
+
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int c = int.Parse(Console.ReadLine());
+
+int max = a;
+
+if (b>max)max=b;
+if (c>max)max=c;
+
+Console.WriteLine("Наибольшее число: {0}", max);
