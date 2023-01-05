4c219c061696a4207d6749f77b9398efb76053dd
Falcon3590 <118375336+Falcon3590@users.noreply.github.com>
2023-01-05 22:14:34 +0300

    Homework1

--- lesson/Program.cs
+++ lesson/Program.cs
@@ -1,1 +1,55 @@
-﻿
+﻿//Это комментарий, пишу и объясняю как работает мой код
+//Console.WriteLine("Hello World");
+
+// Напишите программу, которая на вход принимает число и выдает его квадрат (число умноженное 
+// на само себя).
+// Например:
+// 4 -> 16 
+// -3 -> 9 
+// -7 -> 49
+
+// Console.Clear();
+// Console.WriteLine("Введите целое число "); // Пользователь вводит число
+// int x = int.Parse(Console.ReadLine()); // Перевели данные от пользователя в целое  
+// int y = x * x;
+// Console.WriteLine($"Квадрат введеного числа равен {y}");
+// if (x > 9 && x < 100)
+// {
+//     Console.WriteLine("Вы ввели положительное двухзначное число!");
+// }
+// Console.WriteLine();
+
+
+// Задача №1. Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
+
+// Console.WriteLine(" Введите число 1  ");
+
+// int x = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine(" Введите число 2  ");
+
+// int y = Convert.ToInt32(Console.ReadLine());
+
+// if (x == y * y)
+// {
+//     Console.WriteLine(" Первое число является кватдратом второго ");
+
+// }
+// else
+// {
+//     Console.WriteLine(" Первое число не является кватратом второго ");
+
+
+
+// Console.WriteLine (" Введите число от 1 до 7");
+// int x = Convert.ToInt32 (Console.ReadLine());
+// if (x == 1) Console.WriteLine ("Понедельник");
+// else if (x == 2) Console.WriteLine ("Вторник");
+// else if (x == 3) Console.WriteLine ("Среда");
+// else if (x == 4) Console.WriteLine ("Четверг");
+// else if (x == 5) Console.WriteLine ("Пятница");
+// else if (x == 6) Console.WriteLine ("Суббота");
+// else if (x == 7) Console.WriteLine ("Воскр");
+// else Console.WriteLine (" Некорректное число");
+
+
